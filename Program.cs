7e5c56cf56c274302392f3da85a6c450a42fd74b
ZhilenkovAndrey <dron19812013@gmail.com>
2023-04-06 12:52:11 +0300

    task3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
-﻿//HomeWork1 Task2//
-int max;
+﻿//HomeWork1//
+
+
+// Task2//
+
+// int max;
+
+// Console.Write("Enter a num1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter a num2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 >= num2) 
+//     Console.WriteLine($"max = {num1}");
+// else
+//     Console.WriteLine($"max = {num2}"); 
+    
+
+//Task3//
 
 Console.Write("Enter a num1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -7,7 +25,14 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter a num2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 >= num2) 
+Console.Write("Enter a num1: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3) 
     Console.WriteLine($"max = {num1}");
+
+if (num2 > num1 && num2 >num3)    
+    Console.WriteLine($"max = {num2}");
+
 else
-    Console.WriteLine($"max = {num2}"); 
+     Console.WriteLine($"max = {num3}"); 

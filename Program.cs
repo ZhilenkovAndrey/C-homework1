8b6420a037c0131d1783c3731e9b49f7100c47c5
ZhilenkovAndrey <dron19812013@gmail.com>
2023-04-06 13:03:05 +0300

    task6

--- Program.cs
+++ Program.cs
@@ -17,22 +17,37 @@
 //     Console.WriteLine($"max = {num2}"); 
     
 
-//Task3//
+//Task4//
 
-Console.Write("Enter a num1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a num1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter a num2: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a num2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter a num1: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a num1: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2 && num1 > num3) 
-    Console.WriteLine($"max = {num1}");
+// if (num1 > num2 && num1 > num3) 
+//     Console.WriteLine($"max = {num1}");
 
-if (num2 > num1 && num2 >num3)    
-    Console.WriteLine($"max = {num2}");
+// if (num2 > num1 && num2 >num3)    
+//     Console.WriteLine($"max = {num2}");
 
-else
-     Console.WriteLine($"max = {num3}"); 
+// else
+//      Console.WriteLine($"max = {num3}"); 
+
+
+//task6//
+
+bool a;
+
+Console.Write("Enter a num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) 
+    a = true;
+else 
+    a = false;
+
+Console.WriteLine(a);

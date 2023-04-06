ea0337db81a1b0db355f2122a9b4e08abf28a48d
ZhilenkovAndrey <dron19812013@gmail.com>
2023-04-06 12:21:22 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//HomeWork1 Task2//
+int max;
+
+Console.Write("Enter a num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2) 
+    Console.WriteLine($"max = {num1}");
+else
+    Console.WriteLine($"max = {num2}"); 
